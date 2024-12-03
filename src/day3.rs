8897064e4da73069de36b03cf79f587f0f38d40b
Fela Maslen <felamaslen@gmail.com>
2024-12-03 07:37:41 +0000

    feat: day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -5,63 +5,76 @@ pub struct Day3 {
     pub part2: i32,
 }
 
+fn scan_program(input: String, with_do_dont: bool) -> i32 {
+    let mut sum = 0;
+    let mut last_char: Option<char> = None;
+    let mut is_enabled = true;
+    let mut left: Option<String> = None;
+    let mut right: Option<String> = None;
+
+    let mut skip = 0;
+
+    for (i, c) in input.chars().enumerate() {
+        if skip > 0 {
+            skip -= 1;
+            continue;
+        }
+
+        if c == 'd' && with_do_dont {
+            if input[i..i + 4].to_owned() == "do()" {
+                is_enabled = true;
+                skip = 3;
+            } else if input[i..i + 7].to_owned() == "don't()" {
+                is_enabled = false;
+                skip = 6;
+            }
+        }
+        if !is_enabled {
+            continue;
+        }
+        if c == 'm' && input[i..i + 4].to_owned() == "mul(" {
+            last_char = Some('(');
+            left = None;
+            right = None;
+            skip = 3;
+        } else if let Some(l) = last_char {
+            if l == '(' {
+                if c == ',' && left.is_some() {
+                    last_char = Some(',');
+                } else if c.is_digit(10) {
+                    left = Some(format!("{}{}", left.unwrap_or("".to_owned()), c).to_owned());
+                } else {
+                    last_char = None;
+                }
+            } else if l == ',' {
+                if c == ')' {
+                    match (left.clone(), right.clone()) {
+                        (Some(lv), Some(rv)) => {
+                            let value_left = str::parse::<i32>(&lv).unwrap();
+                            let value_right = str::parse::<i32>(&rv).unwrap();
+
+                            sum += value_left * value_right;
+                        }
+                        _ => {}
+                    };
+                    last_char = None;
+                } else if c.is_digit(10) {
+                    right = Some(format!("{}{}", right.unwrap_or("".to_owned()), c).to_owned());
+                } else {
+                    last_char = None;
+                }
+            }
+        }
+    }
+
+    sum
+}
+
 pub fn day3(input: String) -> Day3 {
-    let part1 = input
-        .chars()
-        .fold(
-            (0, None, None, None),
-            |(sum, last_char, left, right): (i32, Option<char>, Option<String>, Option<String>),
-             c| {
-                if c == 'm'
-                    || last_char.is_some_and(|l| {
-                        (c == 'u' && l == 'm') || (c == 'l' && l == 'u') || (c == '(' && l == 'l')
-                    })
-                {
-                    return (sum, Some(c), None, None);
-                }
-                if last_char.is_some_and(|l| l == '(') {
-                    if c == ',' && left.is_some() {
-                        return (sum, Some(','), left, None);
-                    }
-                    if c.is_digit(10) {
-                        return (
-                            sum,
-                            last_char,
-                            Some(format!("{}{}", left.unwrap_or("".to_owned()), c).to_owned()),
-                            None,
-                        );
-                    }
-                    return (sum, None, None, None);
-                }
-                if last_char.is_some_and(|l| l == ',') {
-                    if c == ')' {
-                        return match (left, right) {
-                            (Some(l), Some(r)) => {
-                                let value_left = str::parse::<i32>(&l).unwrap();
-                                let value_right = str::parse::<i32>(&r).unwrap();
+    let part1 = scan_program(input.to_owned(), false);
+    let part2 = scan_program(input.to_owned(), true);
 
-                                return (sum + value_left * value_right, None, None, None);
-                            }
-                            _ => (sum, None, None, None),
-                        };
-                    }
-
-                    if c.is_digit(10) {
-                        return (
-                            sum,
-                            last_char,
-                            left,
-                            Some(format!("{}{}", right.unwrap_or("".to_owned()), c).to_owned()),
-                        );
-                    }
-                    return (sum, None, None, None);
-                }
-                return (sum, None, None, None);
-            },
-        )
-        .0;
-
-    Day3 { part1, part2: 0 }
+    Day3 { part1, part2 }
 }
 
 #[cfg(test)]
@@ -75,5 +88,14 @@ mod tests {
         let result = day3(input.to_owned());
 
         assert_eq!(result.part1, 161);
+    }
+
+    #[test]
+    fn gets_part2() {
+        let input = r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+        let result = day3(input.to_owned());
+
+        assert_eq!(result.part2, 48);
     }
 }
