ded711153181fa85996ca8162560ece34a5e3c78
Fela Maslen <felamaslen@gmail.com>
2024-12-10 07:48:53 +0000

    feat: day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 pub struct Day10 {
     pub part1: usize,
+    pub part2: usize,
 }
 
 struct Board<'a> {
@@ -128,9 +129,47 @@ fn part1(input: String) -> usize {
     })
 }
 
+fn part2(input: String) -> usize {
+    let cells = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|ch| str::parse::<usize>(ch.to_string().as_str()).unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<Vec<usize>>>();
+
+    let height = cells.len();
+    let width = cells[0].len();
+
+    let board = Board {
+        height,
+        width,
+        cells: &cells,
+    };
+
+    let zeroes = cells
+        .iter()
+        .enumerate()
+        .flat_map(|(j, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, v)| **v == 0)
+                .map(move |(i, _)| (i, j))
+        })
+        .collect::<Vec<(usize, usize)>>();
+
+    zeroes.iter().fold(0, |score, zero| {
+        let trails = trails_from_point(&board, zero, 4, vec![*zero]);
+
+        score + trails.len()
+    })
+}
+
 pub fn day10(input: String) -> Day10 {
     Day10 {
-        part1: part1(input),
+        part1: part1(input.clone()),
+        part2: part2(input.clone()),
     }
 }
 
@@ -152,5 +191,21 @@ mod tests {
         let result = day10(input.to_owned());
 
         assert_eq!(result.part1, 36);
+    }
+
+    #[test]
+    fn gets_part2() {
+        let input = r"89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
+
+        let result = day10(input.to_owned());
+
+        assert_eq!(result.part2, 81);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -132,6 +132,7 @@ fn main() {
             let result = day10(input);
             println!("Day 10 result:");
             println!("Part 1: {}", result.part1);
+            println!("Part 2: {}", result.part2);
         }
         _ => panic!("Unknown or unfinished day {}", cli.day),
     }
