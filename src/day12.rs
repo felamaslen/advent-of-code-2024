df093244405188b794fa6963d592957677da696b
Fela Maslen <felamaslen@gmail.com>
2024-12-12 19:19:24 +0000

    feat: day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,22 +1,22 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day12 {
-    pub part1: usize,
-    pub part2: usize,
+    pub part1: isize,
+    pub part2: isize,
 }
 
 struct Board<'a> {
     cells: &'a Vec<Vec<char>>,
-    width: usize,
-    height: usize,
+    width: isize,
+    height: isize,
 }
 
 fn find_region(
     board: &Board,
-    recorded: &mut HashSet<(usize, usize)>,
-    region: &mut HashSet<(usize, usize)>,
+    recorded: &mut HashSet<(isize, isize)>,
+    region: &mut HashSet<(isize, isize)>,
     ch: &char,
-    (i, j): (usize, usize),
+    (i, j): (isize, isize),
 ) {
     let Board {
         cells,
@@ -26,21 +26,23 @@ fn find_region(
 
     region.insert((i, j));
     recorded.insert((i, j));
-    if j > 0 && cells[j - 1][i] == *ch && !region.contains(&(i, j - 1)) {
+    if j > 0 && cells[(j - 1) as usize][i as usize] == *ch && !region.contains(&(i, j - 1)) {
         find_region(board, recorded, region, ch, (i, j - 1));
     }
-    if i < width - 1 && cells[j][i + 1] == *ch && !region.contains(&(i + 1, j)) {
+    if i < width - 1 && cells[j as usize][(i + 1) as usize] == *ch && !region.contains(&(i + 1, j))
+    {
         find_region(board, recorded, region, ch, (i + 1, j));
     }
-    if j < height - 1 && cells[j + 1][i] == *ch && !region.contains(&(i, j + 1)) {
+    if j < height - 1 && cells[(j + 1) as usize][i as usize] == *ch && !region.contains(&(i, j + 1))
+    {
         find_region(board, recorded, region, ch, (i, j + 1));
     }
-    if i > 0 && cells[j][i - 1] == *ch && !region.contains(&(i - 1, j)) {
+    if i > 0 && cells[j as usize][(i - 1) as usize] == *ch && !region.contains(&(i - 1, j)) {
         find_region(board, recorded, region, ch, (i - 1, j));
     }
 }
 
-fn part1(input: String) -> usize {
+fn part1(input: String) -> isize {
     let mut recorded = HashSet::new();
     let mut regions = vec![];
 
@@ -49,8 +51,8 @@ fn part1(input: String) -> usize {
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    let height = cells.len();
-    let width = cells[0].len();
+    let height = cells.len() as isize;
+    let width = cells[0].len() as isize;
 
     let board = Board {
         cells: &cells,
@@ -60,37 +62,43 @@ fn part1(input: String) -> usize {
 
     cells.iter().enumerate().for_each(|(j, line)| {
         line.iter().enumerate().for_each(|(i, ch)| {
-            if recorded.contains(&(i, j)) {
+            if recorded.contains(&(i as isize, j as isize)) {
                 return;
             }
 
             let mut region = HashSet::new();
-            find_region(&board, &mut recorded, &mut region, ch, (i, j));
+            find_region(
+                &board,
+                &mut recorded,
+                &mut region,
+                ch,
+                (i as isize, j as isize),
+            );
 
             regions.push((region, ch));
         });
     });
 
     regions.iter().fold(0, |sum, (region, ch)| {
-        let area = region.len();
+        let area = region.len() as isize;
 
         let perimiter = region.iter().fold(0, |p, (i, j)| {
-            let border_north = if *j == 0 || cells[j - 1][*i] != **ch {
+            let border_north = if *j == 0 || cells[(j - 1) as usize][*i as usize] != **ch {
                 1
             } else {
                 0
             };
-            let border_east = if *i == width - 1 || cells[*j][i + 1] != **ch {
+            let border_east = if *i == width - 1 || cells[*j as usize][(i + 1) as usize] != **ch {
                 1
             } else {
                 0
             };
-            let border_south = if *j == height - 1 || cells[j + 1][*i] != **ch {
+            let border_south = if *j == height - 1 || cells[(j + 1) as usize][*i as usize] != **ch {
                 1
             } else {
                 0
             };
-            let border_west = if *i == 0 || cells[*j][i - 1] != **ch {
+            let border_west = if *i == 0 || cells[*j as usize][(i - 1) as usize] != **ch {
                 1
             } else {
                 0
@@ -103,10 +111,95 @@ fn part1(input: String) -> usize {
     })
 }
 
+fn part2(input: String) -> isize {
+    let mut recorded = HashSet::new();
+    let mut regions = vec![];
+
+    let cells = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    let height = cells.len() as isize;
+    let width = cells[0].len() as isize;
+
+    let board = Board {
+        cells: &cells,
+        width,
+        height,
+    };
+
+    cells.iter().enumerate().for_each(|(j, line)| {
+        line.iter().enumerate().for_each(|(i, ch)| {
+            if recorded.contains(&(i as isize, j as isize)) {
+                return;
+            }
+
+            let mut region = HashSet::new();
+            find_region(
+                &board,
+                &mut recorded,
+                &mut region,
+                ch,
+                (i as isize, j as isize),
+            );
+
+            regions.push((region, ch));
+        });
+    });
+
+    regions.iter().fold(0, |sum, (region, ch)| {
+        let area = region.len() as isize;
+
+        let mut perimiter: HashMap<(i32, isize, isize), Vec<isize>> = HashMap::new();
+
+        region.iter().for_each(|(i, j)| {
+            let border_north = *j == 0 || cells[(j - 1) as usize][*i as usize] != **ch;
+            let border_east = *i == width - 1 || cells[*j as usize][(i + 1) as usize] != **ch;
+            let border_south = *j == height - 1 || cells[(j + 1) as usize][*i as usize] != **ch;
+            let border_west = *i == 0 || cells[*j as usize][(i - 1) as usize] != **ch;
+
+            if border_north {
+                let key = (0, j - 1, *j);
+                (*perimiter.entry(key).or_insert(vec![])).push(*i);
+            }
+            if border_east {
+                let key = (1, i + 1, *i);
+                (*perimiter.entry(key).or_insert(vec![])).push(*j);
+            }
+            if border_south {
+                let key = (0, j + 1, *j);
+                (*perimiter.entry(key).or_insert(vec![])).push(*i);
+            }
+            if border_west {
+                let key = (1, i - 1, *i);
+                (*perimiter.entry(key).or_insert(vec![])).push(*j);
+            }
+        });
+
+        let sides = perimiter.iter_mut().fold(0, |s0, (_, count)| {
+            count.sort_by(|a, b| a.cmp(b));
+
+            count
+                .iter()
+                .fold((count[0], s0), |(prev, s1), next| {
+                    if *next == prev + 1 {
+                        (*next, s1)
+                    } else {
+                        (*next, s1 + 1)
+                    }
+                })
+                .1
+        });
+
+        sum + area * sides
+    })
+}
+
 pub fn day12(input: String) -> Day12 {
     Day12 {
         part1: part1(input.clone()),
-        part2: 0,
+        part2: part2(input.clone()),
     }
 }
 
@@ -129,5 +222,22 @@ MMMISSJEEE";
         let result = day12(input.to_owned());
 
         assert_eq!(result.part1, 1930);
+    }
+
+    #[test]
+    fn gets_part2() {
+        let input = r"RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE";
+        let result = day12(input.to_owned());
+
+        assert_eq!(result.part2, 1206);
     }
 }
